86de40b78ef04b6efd446cb3ce3a001bd29b390e
Administrator <Administrator@PC-20170519HPDC>
2018-11-15 17:49:00 +0800

    修改提交

---
+++ wfk.process/wfk.process.dao/src/main/java/wfk/process/dao/sql/entity/WFKProduct.java
@@ -0,0 +1,295 @@
+package wfk.process.dao.sql.entity;
+
+import java.util.Date;
+
+public class WFKProduct {
+    private Integer id;
+
+    private String productName;
+
+    private String panelBrand;
+
+    private String panelModel;
+
+    private String panelSize;
+
+    private String panelType;
+
+    private String resolution;
+
+    private String pixelFormat;
+
+    private String displayArea;
+
+    private String outlineSize;
+
+    private String surface;
+
+    private String brightness;
+
+    private String contrastRatio;
+
+    private String viewingAngle;
+
+    private String displayMode;
+
+    private String bestView;
+
+    private String responseTime;
+
+    private String displayColor;
+
+    private String lampType;
+
+    private String frequency;
+
+    private String touchScreen;
+
+    private String shapeStyle;
+
+    private String application;
+
+    private String signalInterface;
+
+    private String inputVoltage;
+
+    private Integer appType;
+
+    private Integer status;
+
+    private Date addTime;
+
+    private String content;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName == null ? null : productName.trim();
+    }
+
+    public String getPanelBrand() {
+        return panelBrand;
+    }
+
+    public void setPanelBrand(String panelBrand) {
+        this.panelBrand = panelBrand == null ? null : panelBrand.trim();
+    }
+
+    public String getPanelModel() {
+        return panelModel;
+    }
+
+    public void setPanelModel(String panelModel) {
+        this.panelModel = panelModel == null ? null : panelModel.trim();
+    }
+
+    public String getPanelSize() {
+        return panelSize;
+    }
+
+    public void setPanelSize(String panelSize) {
+        this.panelSize = panelSize == null ? null : panelSize.trim();
+    }
+
+    public String getPanelType() {
+        return panelType;
+    }
+
+    public void setPanelType(String panelType) {
+        this.panelType = panelType == null ? null : panelType.trim();
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution == null ? null : resolution.trim();
+    }
+
+    public String getPixelFormat() {
+        return pixelFormat;
+    }
+
+    public void setPixelFormat(String pixelFormat) {
+        this.pixelFormat = pixelFormat == null ? null : pixelFormat.trim();
+    }
+
+    public String getDisplayArea() {
+        return displayArea;
+    }
+
+    public void setDisplayArea(String displayArea) {
+        this.displayArea = displayArea == null ? null : displayArea.trim();
+    }
+
+    public String getOutlineSize() {
+        return outlineSize;
+    }
+
+    public void setOutlineSize(String outlineSize) {
+        this.outlineSize = outlineSize == null ? null : outlineSize.trim();
+    }
+
+    public String getSurface() {
+        return surface;
+    }
+
+    public void setSurface(String surface) {
+        this.surface = surface == null ? null : surface.trim();
+    }
+
+    public String getBrightness() {
+        return brightness;
+    }
+
+    public void setBrightness(String brightness) {
+        this.brightness = brightness == null ? null : brightness.trim();
+    }
+
+    public String getContrastRatio() {
+        return contrastRatio;
+    }
+
+    public void setContrastRatio(String contrastRatio) {
+        this.contrastRatio = contrastRatio == null ? null : contrastRatio.trim();
+    }
+
+    public String getViewingAngle() {
+        return viewingAngle;
+    }
+
+    public void setViewingAngle(String viewingAngle) {
+        this.viewingAngle = viewingAngle == null ? null : viewingAngle.trim();
+    }
+
+    public String getDisplayMode() {
+        return displayMode;
+    }
+
+    public void setDisplayMode(String displayMode) {
+        this.displayMode = displayMode == null ? null : displayMode.trim();
+    }
+
+    public String getBestView() {
+        return bestView;
+    }
+
+    public void setBestView(String bestView) {
+        this.bestView = bestView == null ? null : bestView.trim();
+    }
+
+    public String getResponseTime() {
+        return responseTime;
+    }
+
+    public void setResponseTime(String responseTime) {
+        this.responseTime = responseTime == null ? null : responseTime.trim();
+    }
+
+    public String getDisplayColor() {
+        return displayColor;
+    }
+
+    public void setDisplayColor(String displayColor) {
+        this.displayColor = displayColor == null ? null : displayColor.trim();
+    }
+
+    public String getLampType() {
+        return lampType;
+    }
+
+    public void setLampType(String lampType) {
+        this.lampType = lampType == null ? null : lampType.trim();
+    }
+
+    public String getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(String frequency) {
+        this.frequency = frequency == null ? null : frequency.trim();
+    }
+
+    public String getTouchScreen() {
+        return touchScreen;
+    }
+
+    public void setTouchScreen(String touchScreen) {
+        this.touchScreen = touchScreen == null ? null : touchScreen.trim();
+    }
+
+    public String getShapeStyle() {
+        return shapeStyle;
+    }
+
+    public void setShapeStyle(String shapeStyle) {
+        this.shapeStyle = shapeStyle == null ? null : shapeStyle.trim();
+    }
+
+    public String getApplication() {
+        return application;
+    }
+
+    public void setApplication(String application) {
+        this.application = application == null ? null : application.trim();
+    }
+
+    public String getSignalInterface() {
+        return signalInterface;
+    }
+
+    public void setSignalInterface(String signalInterface) {
+        this.signalInterface = signalInterface == null ? null : signalInterface.trim();
+    }
+
+    public String getInputVoltage() {
+        return inputVoltage;
+    }
+
+    public void setInputVoltage(String inputVoltage) {
+        this.inputVoltage = inputVoltage == null ? null : inputVoltage.trim();
+    }
+
+    public Integer getAppType() {
+        return appType;
+    }
+
+    public void setAppType(Integer appType) {
+        this.appType = appType;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content == null ? null : content.trim();
+    }
+}
